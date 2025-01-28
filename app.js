234761fc44dcc4baebd69c2952761aeb104c110b
yirney p <patriciamosqueramena@gmail.com>
2025-01-27 19:11:04 -0500

    ultima

--- app.js
+++ app.js
@@ -1,1 +1,95 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en 
+// lógica de programación. Aquí deberás desarrollar la lógica para resolver el 
+// problema.
+let amigos = [];
+let listaSorteados = [];
+let sorteoAleatorio =0;
+
+function registrarAmigo() {
+    let ingresaAmigos = document.querySelector('input').value;
+    if (ingresaAmigos =='') {
+        alert('Por favor ingresa un nombre')
+        } else {
+        amigos.push(ingresaAmigos);
+    }
+    limpiarCaja('amigo');
+    mostrarAmigos();
+    return;
+ 
+ 
+}
+
+
+function mostrarAmigos(){
+    
+    let listadoAmigos = document.getElementById('listaAmigos');
+    listadoAmigos.innerHTML = '';
+    
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
+        listadoAmigos.appendChild(li);
+    
+    }
+    return;
+}
+
+function sortearAmigo() {
+    let sorteo = document.getElementById('resultado');
+    sorteo.innerHTML = '';
+    
+    if (amigos =='') {
+        alert ('Debes ingresar personas para el sorteo de Amigo Secreto');
+    } else {
+
+        sorteoAleatorio = amigos[numeroAleatorio()];
+        
+
+        if (amigos.length == listaSorteados.length){
+            alert ('Ya se sortearon todos los amigos, ¡gracias por participar!')
+            reiniciarSorteo();
+        } else { 
+            
+            while(listaSorteados.includes(sorteoAleatorio)){
+                sorteoAleatorio = amigos[numeroAleatorio()];
+            }
+            
+            let amigoSorteado = (`El amigo secreto sorteado es: ${sorteoAleatorio}`);
+            let li = document.createElement('li');
+            li.textContent = amigoSorteado
+            sorteo.appendChild(li);
+            listaSorteados.push(sorteoAleatorio);
+            
+        }
+        
+        let listadoAmigos = document.getElementById('listaAmigos');
+        listadoAmigos.innerHTML = '';
+
+        
+    } 
+
+    return;
+}
+
+function numeroAleatorio() {
+    let aleatorio = Math.floor(Math.random() * amigos.length);
+    return aleatorio;
+}
+
+function limpiarCaja(elemento){
+    document.getElementById(elemento).value =''; 
+    return;
+}
+
+function reiniciarSorteo() {
+    amigos = [];            // Limpiar la lista de amigos
+    listaSorteados = [];    // Limpiar la lista de sorteados
+
+    // Limpiar la interfaz para que quede vacía
+    let listadoAmigos = document.getElementById('listaAmigos');
+    listadoAmigos.innerHTML = '';
+
+    let sorteoResultado = document.getElementById('resultado');
+    sorteoResultado.innerHTML = '';
+    return;
+}
